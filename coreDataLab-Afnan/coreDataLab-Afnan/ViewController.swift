470c5a4c42c3b68455296f02aaf8d90223a172d8
Afnan khalid <Afnanmoghram@gmail.com>
2021-12-21 09:56:31 +0300

    complete the coreData Model

---
+++ coreDataLab-Afnan/coreDataLab-Afnan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataLab-Afnan
+//
+//  Created by Fno Khalid on 15/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
