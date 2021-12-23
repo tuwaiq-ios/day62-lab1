28b543fb3bf083c1026c1f80f76dd76967c24f6d
mohammedali111 <91420952+mohammedali111@users.noreply.github.com>
2021-12-23 10:00:49 +0300

    Modael of core data

---
+++ CD-Lab/CD-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CD-Lab
+//
+//  Created by Sara M on 15/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
