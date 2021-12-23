1832c33304b30c1f44b63bac54f44f7d0954733b
Jawaher <jawaher.hass@gmail.com>
2021-12-24 00:35:51 +0300

    ✔️

---
+++ Lab/Lab/Part/PartVC.swift
@@ -0,0 +1,18 @@
+//
+//  PartVC.swift
+//  Lab
+//
+//  Created by Jawaher🌻 on 19/05/1443 AH.
+//
+
+import Foundation
+import UIKit
+class PartVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
