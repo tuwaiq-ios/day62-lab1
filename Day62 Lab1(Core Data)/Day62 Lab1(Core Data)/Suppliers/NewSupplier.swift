d60c8a3c8ea36c4d4918d973bbec7d128df696a1
Ibrahim Asiri <i-m-6@hotmail.com>
2021-12-29 10:26:52 +0300

    CoreDataLab

---
+++ Day62 Lab1(Core Data)/Day62 Lab1(Core Data)/Suppliers/NewSupplier.swift
@@ -0,0 +1,74 @@
+
+import UIKit
+
+class NewSupplier: UIViewController, UITextFieldDelegate {
+    
+    
+    var data: Supplier?
+    
+    lazy var contentTF: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Add name .."
+        tf.delegate = self
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.black.cgColor
+        tf.layer.cornerRadius = 25
+       
+        return tf
+    }()
+    
+    lazy var contentTF2: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Add website"
+        tf.delegate = self
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.black.cgColor
+        tf.layer.cornerRadius = 25
+        return tf
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title =  NSLocalizedString("Add new supplier", comment:"")
+        view.backgroundColor = UIColor( #colorLiteral(red: 0, green: 0.4206840694, blue: 0.3233159482, alpha: 1))
+       
+        view.backgroundColor = .white
+        view.addSubview(contentTF)
+        view.addSubview(contentTF2)
+      
+        NSLayoutConstraint.activate([
+            contentTF.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            contentTF.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            contentTF.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            contentTF.heightAnchor.constraint(equalToConstant: 50),
+            
+            
+            contentTF2.topAnchor.constraint(equalTo: contentTF.bottomAnchor, constant: 30),
+            contentTF2.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            contentTF2.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            contentTF2.heightAnchor.constraint(equalToConstant: 50),
+        
+        ])
+        contentTF.text = data?.name
+        contentTF2.text = data?.website?.absoluteString
+    
+}
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        contentTF.resignFirstResponder()
+        contentTF2.resignFirstResponder()
+        
+        let supplier = Supplier(
+            context: DataService.shared.viewContext
+        )
+        supplier.name = contentTF.text ?? ""
+        supplier.website = URL(string: contentTF2.text ?? "")
+        
+        DataService.shared.saveContext()
+        
+        return true
+    }
+}
